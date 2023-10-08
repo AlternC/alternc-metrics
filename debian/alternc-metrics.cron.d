369d1e572d0bd181f72827281cf46ce22a6329c3
Benjamin Sonntag <benjamin@octopuce.fr>
2023-10-08 13:14:16 +0200

    [dev] adding a check on systemd running to prevent duplicate cron running

--- debian/alternc-metrics.cron.d
+++ debian/alternc-metrics.cron.d
@@ -3,5 +3,6 @@ MAILTO=""
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # every day after the logrotate, launch alternc-metrics collection
-0 7 * * * root cd /usr/lib/alternc && ./cron_metrics.php &>/dev/null
+# only if systemd is not installed (if it is, this package deploys a timer, which has priority)
+0 7 * * * root cd /usr/lib/alternc && if [ ! -d /run/systemd/system ]; then ./cron_metrics.php ; fi  &>/dev/null
 
